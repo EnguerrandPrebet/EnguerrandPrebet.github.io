3df3e4d2fedeb9883121396763a0298a69054cb4
E. Prebet <enguerrand.prebet@kit.edu>
2023-08-30 17:43:25 +0200

    closing bar after click

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.getElementById("toggle-nav").addEventListener("click", function () {
   document.getElementById("main-nav").classList.toggle("active");
 });
+
+document.querySelectorAll("nav a").forEach(function (link) {
+  link.addEventListener("click", function () {
+    document.getElementById("main-nav").classList.remove("active");
+  });
+});
